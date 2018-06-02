17fd10a0db3c1a3ff18ce48bb742fe705b7fbcfc
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-02 14:06:30 +0300

    fix

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 # This is a comment
 git pull origin develop
 docker build -t vchekryzhov/machine-info-server .
-docker push vchekryzhov/machine-info-server:raspberry
 #sudo docker run --rm -it -p 8080:8080 vchekryzhov/machine-info-server
