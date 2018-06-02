0839e87638ab995f8845d176b88eed70786159e6
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-02 15:11:10 +0300

    ашч

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This is a comment
 git -C ~/machine-info-server pull origin develop
 docker build -t vchekryzhov/machine-info-server ~/machine-info-server
-sudo docker run --rm -it -p 8080:8080 vchekryzhov/machine-info-server
+sudo docker run --rm -it --restart=always -p 8080:8080 vchekryzhov/machine-info-server
