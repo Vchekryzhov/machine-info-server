a795c1b48b82b268f37e783ce7f38b2a724c2fe9
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-02 14:33:34 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is a comment
-git pull origin develop
-docker build -t vchekryzhov/machine-info-server .
+git -C ~/machine-server-info pull origin develop
+docker build -t vchekryzhov/machine-info-server ~/machine-server-info
 #sudo docker run --rm -it -p 8080:8080 vchekryzhov/machine-info-server
