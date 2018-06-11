fbde155aa2cdee58d32bdc094f19ac6952fc962e
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-11 18:59:11 +0300

    fix

--- build2run_docker.sh
+++ build2run_docker.sh
@@ -3,4 +3,4 @@
 
 docker build -t vchekryzhov/machine-info-server .
 docker rm $(docker stop $(docker ps -a -q --filter publish=8080 --format="{{.ID}}"))
-sudo docker run --restart=always -p 8080:8080 vchekryzhov/machine-info-server
+sudo reboot
