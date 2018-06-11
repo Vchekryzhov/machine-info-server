ef14a3bae156e675ec07c4614349b5666c8c99a4
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-11 17:59:25 +0300

    fix

--- build2run_docker.sh
+++ build2run_docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Собирает и запускает докер контейнер
+sudo service docker stop
 docker build -t vchekryzhov/machine-info-server .
 sudo docker run  -it --restart=always -p 8080:8080 vchekryzhov/machine-info-server
+sudo service docker run
