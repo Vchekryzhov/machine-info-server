218a4478e0b9ce4f89a718a0d6bfbc60bec8e632
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-11 18:47:01 +0300

    test

--- build2run_docker.sh
+++ build2run_docker.sh
@@ -2,5 +2,5 @@
 # Собирает и запускает докер контейнер
 
 docker build -t vchekryzhov/machine-info-server .
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=vchekryzhov/machine-info-server --format="{{.ID}}"))
+docker rm $(docker stop $(docker ps -a -q --filter publish=8080 --format="{{.ID}}"))
 sudo docker run --restart=always -p 8080:8080 vchekryzhov/machine-info-server
