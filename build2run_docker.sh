cbff493e62a981f6cf385c178b50accd9ca284a9
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-11 19:16:44 +0300

    fix

--- build2run_docker.sh
+++ build2run_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Собирает и запускает докер контейнер
-
-docker build -t vchekryzhov/machine-info-server .
 docker rm $(docker stop $(docker ps -a -q --filter publish=8080 --format="{{.ID}}"))
+docker build -t vchekryzhov/machine-info-server .
 docker run -it --restart=always -p 8080:8080 vchekryzhov/machine-info-server
