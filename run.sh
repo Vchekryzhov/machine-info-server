f38a0580ba762acd582248194533e324e803e9f6
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-11 17:53:57 +0300

    post merge

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo service docker restart
-git -C ~/machine-info-server pull origin develop
-docker build -t vchekryzhov/machine-info-server ~/machine-info-server
+docker build -t vchekryzhov/machine-info-server .
 sudo docker run  -it --restart=always -p 8080:8080 vchekryzhov/machine-info-server
