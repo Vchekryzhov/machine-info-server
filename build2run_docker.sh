9ae366483bc2a75017277e3ca11945ba73212a0f
Viktor <v.chekryzhov@oblakogroup.ru>
2018-06-12 14:19:43 +0300

    fix ports

--- build2run_docker.sh
+++ build2run_docker.sh
@@ -3,4 +3,4 @@
 git pull origin develop
 docker rm $(docker stop $(docker ps -a -q --filter publish=8080 --format="{{.ID}}"))
 docker build -t vchekryzhov/machine-info-server .
-docker run -it --restart=always -p 8080:8080 vchekryzhov/machine-info-server
+docker run -it --restart=always --net="host" -p 8080:8080 -p 5555:5555/udp vchekryzhov/machine-info-server
